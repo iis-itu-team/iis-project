0f950f01972c1eb7fab9c8f27d30959d1957a9fd
Wertik1206 <jregvx@gmail.com>
2023-10-26 20:20:41 +0200

    fix: include & import logo correctly through assets

--- frontend/src/lib/components/NavBar.svelte
+++ frontend/src/lib/components/NavBar.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import logo from "$lib/assets/logo.svg";
 
 	$: pageId = $page.route.id;
 </script>
 
 <nav class="flex flex-row h-20 gap-x-10 border-b-2 border-text">
-	<img src="logo.svg" alt="logo" />
+	<img src={logo} alt="logo" />
 	<div class="w-full flex flex-row justify-between pr-10">
 		<div class="flex flex-row items-center gap-x-10">
 			<a class={pageId === '/' ? 'font-semibold underline' : ''} href="/">home</a>
