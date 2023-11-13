b2490c1bcb186b731d53ac664f6942590ba9b6b1
Matúš Hubinský <matus.hubinsky@scot.sk>
2023-11-13 22:45:50 +0100

    fix(BE): add HTTP context

--- backend/app/Controllers/AuthController.ts
+++ backend/app/Controllers/AuthController.ts
@@ -1,13 +1,15 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from "App/Models/User"
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class AuthController {
 
-    public async register({ request, response }) {
+    register({ request, response }: HttpContextContract) {
         const { nickname, password, email } = request.all()
 
         try {
-            const user = await User.create({ nickname, email, password })
+            const user = User.create({ nickname, email, password })
+            console.log(user)
             return response.send({ message: 'New account was created successfully' })
         } catch (error) {
             console.log(error)
