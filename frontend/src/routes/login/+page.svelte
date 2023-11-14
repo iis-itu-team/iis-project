b5228b49aab856eea95d2722d48e58323ab523b3
Matúš Hubinský <matus.hubinsky@scot.sk>
2023-11-14 19:09:40 +0100

    style(FE): more compact style

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -12,8 +12,6 @@
 				},
 				body: JSON.stringify({ nickname, password })
 			});
-			const data = await response.json();
-			alert(data.message);
 		} catch (error) {
 			console.error('An error occurred:', error);
 			alert('An error occurred. Please check the console for details.');
@@ -22,27 +20,25 @@
 </script>
 
 <form on:submit={login}>
-    <div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-10 max-w-sm">
+    <div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-5 max-w-sm">
         <p>Enter your Nickname:</p>
-		<input type="text" class="text-input" bind:value={nickname} required />
+		<input type="text" class="text-input w-full" bind:value={nickname} required />
 	</div>
 
-    <div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-10 max-w-sm">
+    <div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-5 max-w-sm">
         <p>Password:</p>
-		<input type="password" class="text-input" bind:value={password} required />
+		<input type="password" class="text-input w-full" bind:value={password} required />
 	</div>
 
 	<div class="mx-auto max-w-sm">
 		<p><a href="/password_reset">Forgot your password?</a></p>
 	</div>
 
-	<button type="submit" class="bg-primary hover:bg-secondary box-border rounded-xl p-6 mx-auto mx-10 my-10 max-w-sm flex items-center">
-		<p>
-			<b>Log in</b>
-		</p>
+	<button type="submit" class="bg-primary hover:bg-secondary box-border rounded-xl p-6 mx-auto mx-10 my-5 w-full max-w-sm flex items-center justify-center text-2xl">
+		<p><b>Log in</b></p>
 	</button>
 
-	<div class="mx-auto mx-10 my-10 max-w-sm">
+	<div class="mx-auto mx-10 my-5 max-w-sm">
 		<label>
 			<input type="checkbox" checked="unchecked" name="remember" />Remember me
 		</label>
--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -23,35 +23,33 @@
 
 <form on:submit={register}>
 	<div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-5 max-w-sm">
-		<p>Choose a uniqe nickname:</p>
-		<input type="text" class="text-input" placeholder="Enter your Username" name="nickname" required/>
+		<p><b>Choose a uniqe nickname:<b></p>
+		<input type="text" class="text-input w-full" placeholder="Enter your Username" name="nickname" required/>
 	</div>
 
 	<div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-5 max-w-sm">
 		<p>Write your email:</p>
-		<input type="email" class="text-input" placeholder="Enter your Email" name="email" required />
+		<input type="email" class="text-input w-full" placeholder="Enter your Email" name="email" required />
 	</div>
 
 	<div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-5 max-w-sm">
 		<p>Verify email:</p>
-		<input type="email" class="text-input" placeholder="Enter your Email again" name="email" required/>
+		<input type="email" class="text-input w-full" placeholder="Enter your Email again" name="email" required/>
 	</div>
 
 	<div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-5 max-w-sm">
 		<p>Create a password:</p>
-		<input type="password" class="text-input" placeholder="Enter your Password" name="password" required minlength="5" maxlength="20"/>
+		<input type="password" class="text-input w-full" placeholder="Enter your Password" name="password" required minlength="5" maxlength="20"/>
 	</div>
 
 	<div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-5 max-w-sm">
 		<p>Verify password:</p>
-		<input type="password" class="text-input" placeholder="Enter your Password again" name="password" required minlength="5" maxlength="20"/>
+		<input type="password" class="text-input w-full" placeholder="Enter your Password again" name="password" required minlength="5" maxlength="20"/>
 	</div>
 
-	<div class="bg-primary hover:bg-secondary box-border rounded-xl p-6 mx-auto mx-10 my-5 max-w-sm flex items-center justify-center">
-		<button type="submit" class="text-2xl">
-			<p><b>Register</b></p>
-		</button>
-	</div>
+	<button type="submit" class="bg-primary hover:bg-secondary box-border rounded-xl p-6 mx-auto mx-10 my-5 w-full max-w-sm flex items-center justify-center text-2xl">
+		<p><b>Register</b></p>
+	</button>
 
 	<div class="mx-auto mx-10 my-5 max-w-sm">
 		<p><a href="/login">Already have a account? Login here!</a></p>
