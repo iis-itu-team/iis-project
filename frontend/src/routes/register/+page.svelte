96ae3c3e930b8f152eeaa6689986bbb241f6b053
Matúš Hubinský <matus.hubinsky@scot.sk>
2023-11-14 09:59:33 +0100

    style(FE): small style change

--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -14,8 +14,6 @@
 				},
 				body: JSON.stringify({ nickname, email, password })
 			});
-			const data = await response.json();
-			alert(data.message);
 		} catch (error) {
 			console.error('An error occurred:', error);
 			alert('An error occurred. Please check the console for details.');
@@ -24,38 +22,38 @@
 </script>
 
 <form on:submit={register}>
-	<div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-10 max-w-sm">
+	<div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-5 max-w-sm">
 		<p>Choose a uniqe nickname:</p>
 		<input type="text" class="text-input" placeholder="Enter your Username" name="nickname" required/>
 	</div>
 
-	<div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-10 max-w-sm">
+	<div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-5 max-w-sm">
 		<p>Write your email:</p>
 		<input type="email" class="text-input" placeholder="Enter your Email" name="email" required />
 	</div>
 
-	<div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-10 max-w-sm">
+	<div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-5 max-w-sm">
 		<p>Verify email:</p>
 		<input type="email" class="text-input" placeholder="Enter your Email again" name="email" required/>
 	</div>
 
-	<div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-10 max-w-sm">
+	<div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-5 max-w-sm">
 		<p>Create a password:</p>
 		<input type="password" class="text-input" placeholder="Enter your Password" name="password" required minlength="5" maxlength="20"/>
 	</div>
 
-	<div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-10 max-w-sm">
+	<div class="bg-secondary rounded-xl p-6 mx-auto mx-10 my-5 max-w-sm">
 		<p>Verify password:</p>
 		<input type="password" class="text-input" placeholder="Enter your Password again" name="password" required minlength="5" maxlength="20"/>
 	</div>
 
-	<div class="bg-primary hover:bg-secondary box-border rounded-xl p-6 mx-auto mx-10 my-10 max-w-sm flex items-center justify-center">
+	<div class="bg-primary hover:bg-secondary box-border rounded-xl p-6 mx-auto mx-10 my-5 max-w-sm flex items-center justify-center">
 		<button type="submit" class="text-2xl">
 			<p><b>Register</b></p>
 		</button>
 	</div>
 
-	<div class="mx-auto mx-10 my-10 max-w-sm">
+	<div class="mx-auto mx-10 my-5 max-w-sm">
 		<p><a href="/login">Already have a account? Login here!</a></p>
 	</div>
 </form>
