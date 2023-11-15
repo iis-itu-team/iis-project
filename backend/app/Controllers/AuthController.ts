463562cb994e055c9af9294e9b1ec9ccb27c7737
Matúš Hubinský <matus.hubinsky@scot.sk>
2023-11-15 12:16:12 +0100

    fix(be): auth, added login with email or nickname

--- backend/app/Controllers/AuthController.ts
+++ backend/app/Controllers/AuthController.ts
@@ -3,24 +3,28 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class AuthController {
 
-    public async register({ request, response }: HttpContextContract) {
+    public async register({ request, response, auth }: HttpContextContract) {
         const { nickname, password, email } = request.all()
 
         // TODO: check if the email is already taken
         // TODO: Move that logic in to a service (App\Services)
 
-        const user = await User.create({ nickname, email, password })
-
-        response.status(201).success(user)
+        try {
+            const user = await User.create({ nickname, email, password })
+            await auth.login(user)
+            return response.status(201).success(user)
+        } catch (error) {
+            response.status(401).formatted({ status: 'user_cannot_be_created' })
+        }
     }
 
-    public async login({ request, auth, response }: HttpContextContract) {
-        const { email, password } = request.all()
+    public async login({ request, response, auth }: HttpContextContract) {
+        const { uid, password } = request.all()
 
         // TODO: Check whether the user with this email exists
 
         try {
-            const user = await auth.attempt(email, password)
+            const user = await auth.attempt(uid, password)
 
             response.status(200).formatted({
                 status: 'success',
