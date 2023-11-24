aac336abd4b5e3767e017795af584599728a69dd
Wertik1206 <jregvx@gmail.com>
2023-11-24 14:12:22 +0100

    fix(fe): load group requests only if logged in

--- frontend/src/lib/stores/requests.ts
+++ frontend/src/lib/stores/requests.ts
@@ -2,12 +2,19 @@ import { client } from "$lib/http/http";
 import type { GroupRequest, ResponseFormat } from "$lib/types";
 import { get, writable } from "svelte/store";
 import { toasts } from "svelte-toasts";
+import { currentUser } from "./auth";
 
 // group requests of the currently logged in user
 
 export const groupRequests = writable<GroupRequest[]>([]);
 
 export const fetchRequests = async () => {
+
+    if (!get(currentUser)) {
+        groupRequests.set([]);
+        return;
+    }
+
     const res = await client.get<ResponseFormat<GroupRequest[]>>(`/requests`, {
         params: {
             me: "true"
