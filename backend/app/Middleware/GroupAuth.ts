ae419f1f41df7c392c77a1a0fc7c22051072e378
Marek Vandík <marek.vandik@gmail.com>
2023-11-24 19:08:15 +0100

    fix(BE): group auth bypass for sys admin

--- backend/app/Middleware/GroupAuth.ts
+++ backend/app/Middleware/GroupAuth.ts
@@ -2,9 +2,15 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { Visibility } from 'types/visibility'
 import Group from 'App/Models/Group'
 import Database from '@ioc:Adonis/Lucid/Database'
+import { Role } from 'types/role'
 
 export default class GroupAuth {
   public async handle({ auth, request, response }: HttpContextContract, next: () => Promise<void>) {
+    if (auth.isLoggedIn && auth.user?.role == Role.ADMIN) {
+      await next()
+      return
+    }
+
     const groupId = request.param('group_id') ?? request.param('id')
 
     const group = await Group.query().select('visibility').where('groups.id', '=', groupId)
