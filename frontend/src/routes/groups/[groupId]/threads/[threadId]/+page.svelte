4bb6c73f65ce02bcdc95d77a8a440b38cd9206a3
Wertik1206 <jregvx@gmail.com>
2023-11-26 13:26:07 +0100

    fix(fe): thread page - fix types

--- frontend/src/routes/groups/[groupId]/threads/[threadId]/+page.svelte
+++ frontend/src/routes/groups/[groupId]/threads/[threadId]/+page.svelte
@@ -4,19 +4,20 @@
 	import { currentUser } from '$lib/stores/auth';
 	import { setCrumbs, showCrumbs } from '$lib/stores/breadcrumbs';
 	import type { Group, Message, Thread } from '$lib/types';
+	import type { PageData } from './$types';
 
-	export let data: { group: Group; thread: Thread; messages: Message[] };
+	export let data: PageData;
 
-	$: sorted_messages = data.messages.sort((mes1, mes2) => {
-		let date1 = new Date(mes1.date)
-		let date2 = new Date(mes2.date)
+	$: sorted_messages = data.messages.sort((mes1: Message, mes2: Message) => {
+		let date1 = new Date(mes1.date);
+		let date2 = new Date(mes2.date);
 
 		if (date1 == date2) {
-			return mes1.id > mes2.id	
+			return mes1.id > mes2.id ? 1 : -1;
 		}
 
-		return date1 > date2
-	})
+		return date1 > date2 ? 1 : -1;
+	});
 
 	showCrumbs(true);
 	$: setCrumbs([
@@ -42,7 +43,7 @@
 		}
 
 		client
-			.post(`/groups/${data.group.id}/threads/${data.thread.id}/messages`, {
+			.post(`/groups/${data.group?.id}/threads/${data.thread?.id}/messages`, {
 				content,
 				ownerId: $currentUser?.id
 			})
@@ -52,48 +53,57 @@
 			});
 	};
 
-	async function handleUp(current_message) {
-		await client.post('/messages/' + current_message.id + '/ratings', {
-			message_id: current_message.id,
-			up: 'true'
-		}).then(() => {
-			invalidateAll();
-			content = undefined;
-		});
-	};
+	async function handleUp(current_message: Message) {
+		await client
+			.post('/messages/' + current_message.id + '/ratings', {
+				message_id: current_message.id,
+				up: 'true'
+			})
+			.then(() => {
+				invalidateAll();
+				content = undefined;
+			});
+	}
 
-	async function handleDown(current_message) {
-		await client.post('/messages/' + current_message.id + '/ratings', {
-			message_id: current_message.id,
-			up: 'false'
-		}).then(() => {
-			invalidateAll();
-			content = undefined;
-		});
-	};
-
+	async function handleDown(current_message: Message) {
+		await client
+			.post('/messages/' + current_message.id + '/ratings', {
+				message_id: current_message.id,
+				up: 'false'
+			})
+			.then(() => {
+				invalidateAll();
+				content = undefined;
+			});
+	}
 </script>
 
 <div class="flex flex-col gap-y-2 w-full">
-	<p class="text-white font-semibold text-lg">messages ({data.messages.length}):</p>
+	<p class="text-white font-semibold text-lg">messages ({data.messages?.length}):</p>
 
 	<div class="flex flex-col gap-y-4 border p-4 border-secondary rounded-md">
-		{#if data.messages.length == 0}
+		{#if data.messages?.length == 0}
 			<p>Nothing yet... so empty.</p>
 		{:else}
 			{#each sorted_messages as message}
 				<div class="flex flex-col">
-					<div class="flex flex-row gap-x-2 grid grid-cols-2">
+					<div class="grid grid-cols-2 gap-x-2">
 						<div class="place-self-start">
 							<span class="text-lg font-semibold">{message.owner.nickname}</span>
 							<span>&lt;{message.owner.email}&gt;</span>
 						</div>
 						<div class="flex flex-row items-center space-x-2 place-self-end">
 							<p class="text-white font-bold">{message.rating}</p>
-							<button class="flex bg-primary hover:bg-background-light text-white font-bold py-1 px-2.5 rounded-full" on:click={async () => await handleUp(message)}>
+							<button
+								class="flex bg-primary hover:bg-background-light text-white font-bold py-1 px-2.5 rounded-full"
+								on:click={async () => await handleUp(message)}
+							>
 								&#x21E7
 							</button>
-							<button class="flex bg-secondary hover:bg-background-light text-white font-bold py-1 px-2.5 rounded-full" on:click={async () => await handleDown(message)}>
+							<button
+								class="flex bg-secondary hover:bg-background-light text-white font-bold py-1 px-2.5 rounded-full"
+								on:click={async () => await handleDown(message)}
+							>
 								&#x21E9
 							</button>
 						</div>
@@ -103,12 +113,12 @@
 							{message.content}
 						</p>
 						<p class="text-right">
-							{new Date(message.date).toLocaleString("en-gb", {
-								hour: "2-digit",
-								minute: "2-digit",
-								day: "2-digit",
-								month: "2-digit",
-								year: "numeric"
+							{new Date(message.date).toLocaleString('en-gb', {
+								hour: '2-digit',
+								minute: '2-digit',
+								day: '2-digit',
+								month: '2-digit',
+								year: 'numeric'
 							})}
 						</p>
 					</div>
@@ -134,7 +144,7 @@
 		</div>
 	{:else}
 		<div class="w-full">
-			<span class="italic">Only registered users can send messages.</span>
+			<span class="italic">Only registered users that are group members can send messages.</span>
 		</div>
 	{/if}
 </div>
