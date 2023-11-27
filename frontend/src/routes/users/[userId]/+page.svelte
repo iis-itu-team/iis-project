a5218090c7f4c3b8562e2a036cbb5da6077448f6
Wertik1206 <jregvx@gmail.com>
2023-11-27 13:35:30 +0100

    feat(fe): add nickname change to administration, resolves #47

--- frontend/src/routes/users/[userId]/+page.svelte
+++ frontend/src/routes/users/[userId]/+page.svelte
@@ -4,10 +4,11 @@
 	import { client } from '$lib/http/http';
 	import { currentUser } from '$lib/stores/auth';
 	import { setCrumbs, showCrumbs } from '$lib/stores/breadcrumbs';
-	import { type ResponseFormat, UserRole, type User } from '$lib/types';
+	import { type ResponseFormat, UserRole, type User, type FormFields } from '$lib/types';
 	import { toasts } from 'svelte-toasts';
 	import type { PageData } from './$types';
 	import { invalidateAll } from '$app/navigation';
+	import Form from '$lib/components/Form.svelte';
 
 	export let data: PageData;
 
@@ -62,6 +63,39 @@
 			description: 'Something went wrong.'
 		});
 	};
+
+	const fields: FormFields = {
+		nickname: {
+			validate(val) {
+				if (!val || val.trim().length === 0) {
+					return 'nickname cannot be empty';
+				}
+			},
+			type: 'text',
+			title: 'Nickname'
+		}
+	};
+	$: defaults = {
+		nickname: data.user.nickname
+	};
+
+	let editingNickname = false;
+
+	const handleNickChange = async (values: any) => {
+		const res = await client.put<ResponseFormat<User>>(`/users/${data.user.id}`, values);
+
+		if (res.status === 200 && res.data.status === 'success') {
+			toasts.add({
+				type: 'success',
+				description: 'Users nickname updated.'
+			});
+			invalidateAll();
+			editingNickname = false;
+			return true;
+		}
+
+		return res.data;
+	};
 </script>
 
 <div class="flex flex-col">
@@ -84,14 +118,27 @@
 
 		<!-- admin controls -->
 		{#if $currentUser?.role === UserRole.ADMIN}
-			<h2 class="font-semibold text-lg">Administration</h2>
-			<div class="flex flex-row gap-x-4 p-4">
-				<button
-					class="btn {data.user.role === UserRole.ADMIN && 'btn-disabled'}"
-					disabled={data.user.role === UserRole.ADMIN}
-					on:click={handleMakeAdmin}>make admin</button
-				>
-				<button class="btn-red" on:click={handleDeleteUser}>delete user</button>
+			<h2 class="font-semibold text-lg py-4">Administration</h2>
+			<div class="flex flex-col items-center justify-center">
+				{#if editingNickname}
+					<Form
+						onCancel={() => (editingNickname = false)}
+						onSubmit={handleNickChange}
+						{defaults}
+						{fields}
+						submitText="change"
+					/>
+				{:else}
+					<button class="btn" on:click={() => (editingNickname = true)}>edit nickname</button>
+				{/if}
+				<div class="flex flex-row gap-x-4 p-4">
+					<button
+						class="btn {data.user.role === UserRole.ADMIN && 'btn-disabled'}"
+						disabled={data.user.role === UserRole.ADMIN}
+						on:click={handleMakeAdmin}>make admin</button
+					>
+					<button class="btn-red" on:click={handleDeleteUser}>delete user</button>
+				</div>
 			</div>
 		{/if}
 	</div>
